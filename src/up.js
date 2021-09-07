12ddbd947194c87c90fc7962b803f2ee597525fc
Oleg Grytsay <grytsayo@gmail.com>
2021-09-07 21:32:26 +0300

    loadMore hidden

---
+++ src/up.js
@@ -0,0 +1,23 @@
+const goTopBtn = document.querySelector('.back_to_top');
+
+window.addEventListener('scroll', trackScroll); //отслеживать прокрутку документа
+goTopBtn.addEventListener('click', backToTop);  //отслеживать нажатие на кнопку и приводить в действие скролл «наверх»
+
+function trackScroll() {
+    const scrolled = window.pageYOffset;
+    const coords = document.documentElement.clientHeight;
+//добавляем/удаляем класс back_to_top-show
+    if (scrolled > coords) {
+      goTopBtn.classList.add('back_to_top-show');
+    }
+    if (scrolled < coords) {
+      goTopBtn.classList.remove('back_to_top-show');
+    }
+}
+  
+function backToTop() {
+    if (window.pageYOffset > 0) {
+      window.scrollBy(0, -80);
+      setTimeout(backToTop, 50); //скорость
+    }
+  }
